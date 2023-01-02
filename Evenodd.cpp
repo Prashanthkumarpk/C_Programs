5beab2a6ec87b80c0932fb24a1b21c929c7d7407
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-02 23:05:50 +0530

    Create Evenodd.cpp

---
+++ Evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+    if (n&1) cout<<"odd";
+    else cout<<"even";
+
+    return 0;
+}
+
+enter a number 3
+odd
