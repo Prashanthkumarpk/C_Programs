01550448fd8b91f1da922d0980fc54e7d8c23658
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-02 23:03:47 +0530

    Create PostiveOrNegative.cpp

---
+++ PostiveOrNegative.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+    if (n<0) cout<<"Negtive";
+    else cout<<"positive";
+
+    return 0;
+}
+
+enter a number 20
+positive
