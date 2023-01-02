a4d94fb28d876a3695f5b1e533481b8f0cfa20b7
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-02 23:09:03 +0530

    Create SumOfN.cpp

---
+++ SumOfN.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a number ";
+    std::cin >> n;
+   cout<<"sum "<< n*(n+1)*0.5;
+    
+
+    return 0;
+}
+
+enter a number 5
+sum 15
